e247d5d7e40ec85e8128667658f430cda0fb6ccc
Amith MK <amithmk9@gmail.com>
2020-08-05 22:17:50 +0530

    Initial commit

---
+++ lib/cart_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shopping_cart/cart_bloc.dart';
+import 'package:provider/provider.dart';
+
+class CartPage extends StatelessWidget {
+  CartPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var bloc = Provider.of<CartBloc>(context);
+    var cart = bloc.cart;
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text("Your Cart"),
+      ),
+      body: ListView.builder(
+        itemCount: cart.length,
+        itemBuilder: (context, index) {
+          int giftIndex = cart.keys.toList()[index];
+          int count = cart[giftIndex];
+          return ListTile(
+            leading: Container(
+              height: 70,
+              width: 70,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/${giftIndex + 1}.jpg"),
+                  fit: BoxFit.fitWidth,
+                ),
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+            title: Text('Item Count: $count'),
+            trailing: RaisedButton(
+              child: Text('Clear'),
+              color: Theme.of(context).buttonColor,
+              elevation: 1.0,
+              splashColor: Colors.blueGrey,
+              onPressed: () {
+          bloc.clear(giftIndex);
+          @override
+          Widget build(BuildContext context) {
+          return Align(
+          alignment: Alignment.bottomCenter,
+          child: RaisedButton(
+          onPressed: () {
+          Navigator.pop(
+          context,
+          MaterialPageRoute(
+          builder: (context) => CartPage(),
+          )
+          );
+          },
+          child: const Text('Bottom Button!', style: TextStyle(fontSize: 20)),
+          color: Colors.blue,
+          textColor: Colors.white,
+          elevation: 5,
+    ),
+
+                );
+                }
+
+                },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
